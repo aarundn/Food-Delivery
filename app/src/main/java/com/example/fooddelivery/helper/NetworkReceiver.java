9d8381dbdb41c08d0e0881f1dd6e4a5876fa7184
MCD <debchouneharoun@gmail.com>
2024-08-03 23:46:11 +0100

    network handling commit

--- app/src/main/java/com/example/fooddelivery/helper/NetworkReceiver.java
+++ app/src/main/java/com/example/fooddelivery/helper/NetworkReceiver.java
@@ -1,2 +1,26 @@
-package com.example.fooddelivery.helper;public class NetworkReceiver {
+package com.example.fooddelivery.helper;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkReceiver extends BroadcastReceiver {
+
+    public static ReceiverListener Listener;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ConnectivityManager conn =  (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = conn.getActiveNetworkInfo();
+        if (Listener != null) {
+            boolean isConnected = networkInfo != null && networkInfo.isConnectedOrConnecting();
+            Listener.onNetworkChange(isConnected);
+        }
+    }
+
+    public interface ReceiverListener {
+        void onNetworkChange(boolean isConnected);
+    }
 }
