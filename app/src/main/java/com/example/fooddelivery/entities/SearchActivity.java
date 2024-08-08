d6cbdac2120d4f3f75ad677eac66afc504393fe2
MCD <debchouneharoun@gmail.com>
2024-08-08 14:06:38 +0100

    search design commit1

--- app/src/main/java/com/example/fooddelivery/entities/SearchActivity.java
+++ app/src/main/java/com/example/fooddelivery/entities/SearchActivity.java
@@ -2,11 +2,13 @@ package com.example.fooddelivery.entities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Looper;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
@@ -36,6 +38,7 @@ public class SearchActivity extends AppCompatActivity implements OnItemClickList
     private List<Post> list;
     private ProgressBar searchProgressBar;
     private Timer timer;
+    private ImageView searchIconToBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +49,14 @@ public class SearchActivity extends AppCompatActivity implements OnItemClickList
         searchInput = findViewById(R.id.searchInput);
         itemNotFound = findViewById(R.id.itemNotFound);
         searchProgressBar = findViewById(R.id.searchProgressBar);
+        searchIconToBack = findViewById(R.id.searchIcon);
+
+        searchIconToBack.setImageDrawable(getDrawable(R.drawable.back_left));
+        searchIconToBack.setOnClickListener( v -> {
+            Intent intent = new Intent(SearchActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
 
         searchAndMoreViewModel = new ViewModelProvider(this).get(SearchAndMoreViewModel.class);
         list = new ArrayList<>();
@@ -93,49 +104,39 @@ public class SearchActivity extends AppCompatActivity implements OnItemClickList
 
             @Override
             public void afterTextChanged(Editable s) {
-
-                android.os.Handler handler = new android.os.Handler(Looper.getMainLooper());
-                timer.schedule(new TimerTask() {
-
+                searchProgressBar.setVisibility(View.VISIBLE);
+                itemNotFound.setVisibility(View.GONE);
+                new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
-
-
-                        handler.post(new Runnable() {
-                            @Override
-                            public void run() {searchAdapter.notifyDataSetChanged();
-                            }
+                        searchAndMoreViewModel.searchPosts(s.toString()).observe(SearchActivity.this, postList -> {
+                            searchAdapter.submitList(postList);
                         });
+                        searchAndMoreViewModel.getIsSearchedDataFetched().observe(SearchActivity.this, isDataFetched -> {
+
+                            if (isDataFetched) {
+                                itemNotFound.setVisibility(View.GONE);
+                                searchProgressBar.setVisibility(View.GONE);
+                                searchAdapter.notifyDataSetChanged();
+                            } else {
+                                searchAndMoreViewModel.getIsListEmpty().observe(SearchActivity.this, aBoolean -> {
+                                    if (!s.toString().isEmpty()){
+                                        if (aBoolean){
+                                            itemNotFound.setVisibility(View.VISIBLE);
+                                            searchProgressBar.setVisibility(View.GONE);
+                                        }
+                                    } else {
+                                        itemNotFound.setVisibility(View.GONE);
+                                        searchAdapter.submitList(list);
+                                        searchAdapter.notifyDataSetChanged();
+                                    }
+                                });                    }
+
+                        });
+                        searchAdapter.notifyDataSetChanged();
                     }
                 },500);
-                searchProgressBar.setVisibility(View.VISIBLE);
-                searchAndMoreViewModel.searchPosts(s.toString()).observe(SearchActivity.this, postList -> {
-                    searchAdapter.submitList(postList);
 
-                });
-                searchAndMoreViewModel.getIsSearchedDataFetched().observe(SearchActivity.this, isDataFetched -> {
-
-                    if (isDataFetched) {
-                        itemNotFound.setVisibility(View.GONE);
-                        searchProgressBar.setVisibility(View.GONE);
-                        searchAdapter.notifyDataSetChanged();
-                    } else {
-                        searchAndMoreViewModel.getIsListEmpty().observe(SearchActivity.this, aBoolean -> {
-                            if (!s.toString().isEmpty()){
-                                if (aBoolean){
-                                    itemNotFound.setVisibility(View.VISIBLE);
-                                    searchAdapter.notifyDataSetChanged();
-                                    searchProgressBar.setVisibility(View.GONE);
-                                }
-                            } else {
-                                itemNotFound.setVisibility(View.GONE);
-                                searchAdapter.submitList(list);
-                                searchAdapter.notifyDataSetChanged();
-                            }
-                        });                    }
-
-                });
-                searchAdapter.notifyDataSetChanged();
             }
         });
     }
