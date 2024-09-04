3e89712d3e3057817d88570d5ea99dfad59261b2
MCD <debchouneharoun@gmail.com>
2024-09-04 06:49:33 +0100

    profile image commit

--- app/src/main/java/com/example/fooddelivery/fragments/profileFragment.java
+++ app/src/main/java/com/example/fooddelivery/fragments/profileFragment.java
@@ -1,5 +1,6 @@
 package com.example.fooddelivery.fragments;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,9 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -16,6 +20,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.bumptech.glide.Glide;
+import com.example.fooddelivery.R;
 import com.example.fooddelivery.databinding.FragmentProfileBinding;
 import com.example.fooddelivery.models.User;
 import com.example.fooddelivery.viewmodel.ProfileViewModel;
@@ -31,7 +36,9 @@ public class profileFragment extends Fragment {
     private RoundedImageView profileImagePath;
     private TextView changeText;
     private FrameLayout updateLayoutBtn;
-    private LinearLayout orderLayout, pendingLayout,faqLayout, helpLayout;
+    private LinearLayout orderLayout, pendingLayout, faqLayout, helpLayout;
+    private ActivityResultLauncher<String> pickImage;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -46,35 +53,61 @@ public class profileFragment extends Fragment {
 
         profileViewModel = new ViewModelProvider(requireActivity()).get(ProfileViewModel.class);
 
+        pickImage = registerForActivityResult(
+                new ActivityResultContracts.GetContent(),
+                new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri o) {
+                        binding.profileImage.setImageURI(o);
+                        profileViewModel.addImageToFireBaseStorage(o);
+                    }
+                }
+        );
+
         inputsState(false, binding.userNameProfile);
         inputsState(false, binding.userNumberProfile);
         inputsState(false, binding.userAddressProfile);
         profileViewModel.getGetUserInfo();
-                 profileViewModel.getUserInfo.observe(requireActivity(), user ->{
+        profileViewModel.getUserInfo.observe(requireActivity(), user -> {
                     binding.userNameProfile.setText(user.getUserName());
                     binding.userEmailProfile.setText(user.getEmailAddress());
                     binding.userNumberProfile.setText(user.getPhoneNumber());
                     binding.userAddressProfile.setText(user.getAddress());
-                    if (isAdded()){
-                        Glide.with(requireActivity()).load(user.getImagePath()).into(binding.profileImage);
+                    if (isAdded()) {
+                        if (user.getImagePath() != null) {
+                            Glide.with(requireActivity()).load(user.getImagePath()).into(binding.profileImage);
+                        } else {
+                            binding.profileImage.setImageResource(R.drawable.toy_faces_);
+                        }
                     }
                     inputsState(false, binding.userNameProfile);
                     inputsState(false, binding.userNumberProfile);
                     inputsState(false, binding.userAddressProfile);
+                    binding.profileImage.setFocusable(false);
+                    binding.profileImage.setFocusableInTouchMode(false);
                 }
-                );
+        );
         binding.changeText.setOnClickListener(v -> {
             inputsState(true, binding.userNameProfile);
             inputsState(true, binding.userNumberProfile);
             inputsState(true, binding.userAddressProfile);
+            binding.profileImage.setFocusable(true);
+            binding.profileImage.setFocusableInTouchMode(true);
+            binding.profileImage.setOnClickListener(view1 -> {
+                pickImage.launch("image/*");
+            });
             binding.changeText.setText("save");
             binding.changeText.setOnClickListener(v1 -> {
-                if (inputsCheck()){
-                    User user = new User(binding.userNameProfile.getText().toString(),binding.userAddressProfile.getText().toString(),
-                            "",binding.userNumberProfile.getText().toString());
-                    profileViewModel.modifyUserInfo(user);
+                if (inputsCheck()) {
+                    getImagePath(path -> {
+                        User user = new User(binding.userNameProfile.getText().toString(), binding.userAddressProfile.getText().toString(),
+                                path, binding.userNumberProfile.getText().toString());
+                        profileViewModel.modifyUserInfo(user);
+                    });
+
+
                     profileViewModel.getIsUserAdded().observe(requireActivity(), isAdded -> {
-                        if (isAdded){
+                        if (isAdded) {
                             binding.changeText.setText("Change");
                             inputsState(false, binding.userNameProfile);
                             inputsState(false, binding.userNumberProfile);
@@ -86,16 +119,30 @@ public class profileFragment extends Fragment {
 
         });
     }
+    public interface ImagePathCallback {
+        void onImagePathRetrieved(String path);
+    }
+    private void getImagePath(ImagePathCallback callback) {
+
+        profileViewModel.getIsImageProfileAdded().observe(requireActivity(), isAdded -> {
+            if (isAdded) {
+
+                profileViewModel.downloadImagePath.observe(requireActivity(), callback::onImagePathRetrieved);
+            }
+        });
+
+    }
 
     private void inputsState(boolean b, EditText editText) {
-        if (b){
-           editText.setFocusable(true);
-           editText.setFocusableInTouchMode(true);
+        if (b) {
+            editText.setFocusable(true);
+            editText.setFocusableInTouchMode(true);
         } else {
             editText.setFocusable(false);
             editText.setFocusableInTouchMode(false);
         }
     }
+
 
     private Boolean inputsCheck() {
         if (binding.userNameProfile.getText().toString().isEmpty()) {
--- app/src/main/java/com/example/fooddelivery/viewmodel/ProfileViewModel.java
+++ app/src/main/java/com/example/fooddelivery/viewmodel/ProfileViewModel.java
@@ -1,19 +1,26 @@
 package com.example.fooddelivery.viewmodel;
 
+import android.net.Uri;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.fooddelivery.helper.Constants;
 import com.example.fooddelivery.models.User;
+import com.google.android.gms.auth.api.signin.internal.Storage;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class ProfileViewModel extends ViewModel {
@@ -21,9 +28,13 @@ public class ProfileViewModel extends ViewModel {
     private MutableLiveData<User> _getUserInfo = new MutableLiveData<>();
     public LiveData<User> getUserInfo = _getUserInfo;
     private MutableLiveData<Boolean> isUserAdded = new MutableLiveData<>();
+    private MutableLiveData<String> _downloadImagePath = new MutableLiveData<>();
+    private MutableLiveData<Boolean> isProfileImageAdded = new MutableLiveData<>(false);
+    public MutableLiveData<String> downloadImagePath = _downloadImagePath;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FirebaseAuth auth = FirebaseAuth.getInstance();
     private CollectionReference userReference = db.collection(Constants.USER_COLLECTION);
+    private StorageReference reference = FirebaseStorage.getInstance().getReference();
     public void getGetUserInfo(){
          userReference.document(Objects.requireNonNull(auth.getCurrentUser()).getUid()).get().addOnSuccessListener(task ->  {
 
@@ -31,6 +42,21 @@ public class ProfileViewModel extends ViewModel {
                      _getUserInfo.setValue(user);
 
          }).addOnFailureListener(e -> {
+
+        });
+    }
+
+    public void addImageToFireBaseStorage(Uri imageUri){
+        String imageId = UUID.randomUUID().toString();
+        StorageReference imageRef = reference.child("Files/profile/"+imageId);
+        imageRef.putFile(imageUri).addOnSuccessListener(taskSnapshot -> {
+            imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                downloadImagePath.setValue(uri.toString());
+                isProfileImageAdded.setValue(true);
+            }).addOnFailureListener( e -> {
+
+            });
+        }).addOnFailureListener( e -> {
 
         });
     }
@@ -59,5 +85,8 @@ public class ProfileViewModel extends ViewModel {
 
     public LiveData<Boolean> getIsUserAdded(){
         return isUserAdded;
+    }
+    public LiveData<Boolean> getIsImageProfileAdded(){
+        return isProfileImageAdded;
     }
 }
