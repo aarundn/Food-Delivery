ce436b1e0a27abc7ed8898fd25af1f9414bae33b
MCD <debchouneharoun@gmail.com>
2024-09-06 22:08:27 +0100

    checkout commit

---
+++ app/src/main/java/com/example/fooddelivery/fragments/CheckoutFragment.java
@@ -0,0 +1,34 @@
+package com.example.fooddelivery.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fooddelivery.R;
+import com.example.fooddelivery.databinding.FragmentCheckoutBinding;
+
+public class CheckoutFragment extends Fragment {
+
+    private FragmentCheckoutBinding binding;
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentCheckoutBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
